62a212d2593d1b8938a9b253ff9f1b8e673a1d4e
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-26 12:50:43 -0300

    feat: install and configuration tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Input } from '@/components/ui/input'
+
 export function App() {
   return (
     <div>
       <h1>Koda packages</h1>
+      <Input placeholder="type package name here" />
     </div>
   )
 }
