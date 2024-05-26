2a8cc895d196a0d9537c2bb969407c6b185dd2aa
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-26 17:14:40 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Input } from '@/components/ui/input'
+import { Label } from './components/ui/label'
 
 export function App() {
   return (
-    <div>
-      <h1>Koda packages</h1>
-      <Input placeholder="type package name here" />
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <div className="flex w-full flex-col items-center">
+        <img className="h-24 w-24" src="./koda.svg" alt="logo koda packages" />
+        <Label className="text-md my-4 font-mono tracking-wider text-foreground">
+          koda package
+        </Label>
+        <Input className="w-[300px]" placeholder="type package name here" />
+      </div>
+
+      <small className="fixed bottom-2">made with love by Lucas Frazao</small>
     </div>
   )
 }
